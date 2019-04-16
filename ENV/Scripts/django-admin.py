ad5dd6c0f86f98cc76a029f395fae798298fd5c7
laseur <laseur.r@gmail.com>
2019-04-16 19:33:00 +0200

    virenv

---
+++ ENV/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\robin\desktop\api assignment\test_django_setup_eigen\nieuwe git\django_api2\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
