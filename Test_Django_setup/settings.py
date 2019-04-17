aa8fe64d31b3b3e6dc5f47f26d70bc7338bf3de6
laseur <laseur.r@gmail.com>
2019-04-17 14:14:53 +0200

    heroku

--- Test_Django_setup/settings.py
+++ Test_Django_setup/settings.py
@@ -81,10 +81,7 @@ WSGI_APPLICATION = 'Test_Django_setup.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': os.getenv('DJANGO_DB_URL')
 }
 
 DATABASES['default']['ATOMIC_REQUESTS'] = True
