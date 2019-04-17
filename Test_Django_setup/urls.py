556c4cee9744600166c03ced6b76ddb2c0437067
laseur <laseur.r@gmail.com>
2019-04-17 14:37:31 +0200

    heroku

--- Test_Django_setup/urls.py
+++ Test_Django_setup/urls.py
@@ -22,7 +22,7 @@ from jet_django.urls import jet_urls
 from exact import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('^admin/', admin.site.urls),
     url(r'^exact/', include(('exact.urls', 'exact'), namespace='exact')),
     url(r'^jet_api/', include(jet_urls)),
     url(r'^.*$', views.index, name='index'),
