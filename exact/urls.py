c31116f86a26114e0a348964da64064ec711aa01
laseur <laseur.r@gmail.com>
2019-04-17 15:54:39 +0200

    dag 1 admin werkt nietu

--- exact/urls.py
+++ exact/urls.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 from django.conf.urls import url
 
-from .views import Authenticate, Status, webhook, financials_view, inv_view
+from .views import Authenticate, Status, webhook, financials_view, inv_view, index
 
 urlpatterns = [
+	url(r'^.*$', index, name='index'),
 	url(r'^authenticate$', Authenticate.as_view(), name="authenticate"),
 	url(r'^status', Status.as_view(), name="status"),
 	url(r'^inv', inv_view, name="inv"),
 	url(r'^financials', financials_view, name="financials"),
 	url(r'^webhook', webhook, name="webhook"),
+
 ]
