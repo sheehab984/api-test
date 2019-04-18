02676cb15f5cbd8bfcb9c661da1d518a121fff97
laseur <laseur.r@gmail.com>
2019-04-18 22:03:35 +0200

    Nieuwe home en sneller

--- exact/views.py
+++ exact/views.py
@@ -92,6 +92,29 @@ def webhook(request):
 
 @login_required
 def inv_view(request):
+	e = Exact()
+
+	# first param is the resource.
+	# see https://start.exactonline.nl/docs/HlpRestAPIResources.aspx
+
+	# filter returns a generator and handles pagination for you
+	data1 = []
+	for item in e.filter("inventory/ItemWarehouses",  filter_string=None, select="CurrentStock, ItemCode, ItemDescription"):
+		data1.append(item)
+
+
+	df1 = pd.DataFrame.from_records(data1)
+
+	data2 = []
+	for item in e.filter("logistics/Items", filter_string="substringof('LG', ItemGroupCode) eq true and IsSalesItem eq true", select="Stock, PictureThumbnailUrl, Code"):
+		data2.append(item)
+
+	df2 = pd.DataFrame.from_records(data2)
+
+	data3 = []
+	for item in e.filter("logistics/SalesItemPrices", filter_string="EndDate eq null", select="ItemCode, Price"):
+		data3.append(item)
+
 
 	return render(request, 'exact/inv.html', locals())
 
