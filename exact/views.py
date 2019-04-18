ee288f5eba3268f13024b2f9975358f5fb1fd3c5
laseur <laseur.r@gmail.com>
2019-04-18 19:39:06 +0200

    Nieuwe home en sneller

--- exact/views.py
+++ exact/views.py
@@ -125,10 +125,20 @@ def financials_view(request):
 
 	# filter returns a generator and handles pagination for you
 	data1 = []
-	for item in e.filter("read/financial/ReceivablesListByAccount?accountId=guid'cd8e894a-4a04-47ae-bfd5-da46ef20261c'", select="InvoiceNumber, AccountName, InvoiceDate, InvoiceNumber, Amount, DueDate"):
+	for item in e.filter("read/financial/ReceivablesListByAccount?accountId=guid'cd8e894a-4a04-47ae-bfd5-da46ef20261c'", select="InvoiceNumber, InvoiceDate, InvoiceNumber, Amount, DueDate"):
 		data1.append(item)
 
-	
+	df1 = pd.DataFrame.from_records(data1)
+
+	data2 = []
+	for item in e.filter("documents/Documents", filter_string="substringof('Lichtplanners B.V.', AccountName) eq true", select="SalesInvoiceNumber, ID"):
+		data2.append(item)
+
+	df2 = pd.DataFrame.from_records(data2)
+
+	data3 = []
+	for item in e.filter("documents/DocumentAttachments", select="Document, FileName, Url"):
+		data3.append(item)
 
 	return render(request, 'exact/financials.html', locals())
 
