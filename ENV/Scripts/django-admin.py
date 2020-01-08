4c2475d40d3bcca247ebb2b5c56490da36e2f0c3
sheehab <sheehabpranto@gmail.com>
2020-01-09 03:20:42 +0600

    first commit

--- ENV/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\robin\desktop\api assignment\test_django_setup_eigen\nieuwe git\django_api2\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
